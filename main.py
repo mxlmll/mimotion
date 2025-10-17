b34c9e1c5877cf07ad15912119a2414c5c5c5d30
明镜 <mxlmll@yeah.net>
2025-10-17 21:58:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ def get_min_max_by_time(hour=None, minute=None):
         hour = time_bj.hour
     if minute is None:
         minute = time_bj.minute
-    time_rate = min((hour * 60 + minute) / (22 * 60), 1)
-    min_step = get_int_value_default(config, 'MIN_STEP', 18000)
-    max_step = get_int_value_default(config, 'MAX_STEP', 25000)
+    time_rate = min((max((hour - 6),0) * 60 + minute) / (15 * 60), 1)
+    min_step = get_int_value_default(config, 'MIN_STEP', 16000)
+    max_step = get_int_value_default(config, 'MAX_STEP', 16799)
     return int(time_rate * min_step), int(time_rate * max_step)
 
 
@@ -41,7 +41,7 @@ def fake_ip():
 
 # 账号脱敏
 def desensitize_user_name(user):
-    if len(user) <= 8:
+    if len(user) <= 8:  
         ln = max(math.floor(len(user) / 3), 1)
         return f'{user[:ln]}***{user[-ln:]}'
     return f'{user[:3]}****{user[-4:]}'
